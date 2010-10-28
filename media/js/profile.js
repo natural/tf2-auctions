618ff147608555d5f577d48b100c9d86a0b6aabd
Troy Melhase <troy@gci.net>
2010-10-28 00:50:00 -0800

    profile wip.

--- media/js/profile.js
+++ media/js/profile.js
@@ -21,48 +21,106 @@ var setHeadings = function(prefix) {
     $$('listings-title').text('{0}Recent Listings'.fs( prefix ? prefix+' ' : ''))
 }
 
+var showListing = function(listing, clone) {
+    clone.removeClass('null prototype')
+    if (listing.description) {
+	$('.listing-description', clone).text(listing.description)
+    } else {
+	$('.listing-description-label', clone).empty()
+	$('.listing-description', clone).empty()
+    }
+    $('.listing-owner', clone).text(listing.owner.personaname)
+    $('.listing-avatar', clone).attr('src', listing.owner.avatar)
+    var next = 0
+    $.each(listing.items, function(index, item) {
+	$( $('.item-display div', clone)[next]).append( $.toJSON(item) )
+	next += 1
+    })
+    $$('listings-listings').append(clone)
+}
+
 
 var listingsOkay = function(listings) {
     var llen = listings.length
-    var ltable = $("#own-listings")
-    var proto = $("#own-listings tbody.prototype")
     if (llen > 0) {
-	$('#load-own-msg-listings').text('Success! Showing your ' + llen + ' listing' + (llen==1?'':'s') + '.').fadeOut(5000)
-	ltable.removeClass("null")
+	var proto = $$('listings-inner div.prototype')
+	$.each(listings, function(idx, listing) {
+	    showListing(listing, proto.clone().addClass('listing-wrapper'))
+	})
     } else {
 	$('#load-own-msg-listings').text("You haven't listed any items yet.")
-	// show add listing helper message
     }
-    $.each(listings, function(index, listing) {
-	var c = proto.clone()
-	c.removeClass("null prototype")
-	GL = listing
-	$.each(keys(listing), function(index, key) {
-	    var loc = '.-listing-' + key
-            var val = (listingFormats[key] || listingFormats.any)(listing[key])
-	    $(loc, c).html(val)
-	})
-        console.log('listing:', listing)
-	ltable.append(c)
-    })
     new SchemaTool().setImages()
+    $('div.listing-wrapper td.item-display div:empty').parent().remove()
+}
+
+//
+// backpack
+//
+var backpackShow = function() {
+    smallMsg('Loading backpack...')
+    new BackpackLoader({
+	suffix: id64View(), success: backpackReady, error: backpackError
+    })
+}
+
+var backpackHide = function() {
+    // nb:  this is a nice fadein/fadeout order
+    $$('backpack-hide').fadeOut(function() {
+	$$('backpack-inner').fadeOut(function() {
+	    $$('backpack-show').fadeIn()
+	})
+    })
+}
+
+var backpackReady = function(backpack) {
+    smallMsg('Backpack loaded.')
+    $$('backpack-show').fadeOut(function() { backpackReady__(backpack) })
 }
 
 
-var listingsReady = function(listings) {
-    smallMsg('Listings loaded.')
-    $$('listings-wrapper').slideDown()
-    if (!listings.length < 10) {
-	$$('listings-none').text('Nothing recent.').slideDown()
+var backpackError = function(request, status, error) { }
+
+
+var backpackReady__ = function(backpack) {
+    if (!backpack.profileInit) {
+	var schema = new SchemaTool()
+	var tipTool = new TooltipView(schema)
+	var bpNav = new BackpackNavigator('profile')
+	var bpTool = new BackpackItemsTool(backpack,[], [], 'profile')
+	var hoverItem = function(e) {
+            tipTool.show(e)
+            try {
+		var data = $('img', this).data('node')
+        	if (!data.flag_cannot_trade) {
+	            $(this).addClass('outline')
+                }
+            } catch (e) {}
+	}
+	var unhoverItem = function(e) {
+            tipTool.hide(e)
+            $(this).removeClass('outline')
+	}
+	bpNav.init()
+	bpTool.init()
+	schema.setImages()
+	$$('backpack td').hover(hoverItem, unhoverItem)
+	backpack.profileInit = true
     }
+    $$('backpack-hide').fadeIn()
+    $$('backpack-inner').fadeIn()
+//    $$('backpack-inner').width( $$('backpack-inner table.backpack tbody').first().width() )
 }
 
-var listingsError = function(request, status, error) {
-}
 
+
+
+//
+// bids
+//
 var bidsReady = function(bids) {
     smallMsg('Bids loaded.').fadeOut(1000)
-    $$('bids-wrapper').slideDown()
+    //$$('bids-wrapper').slideDown()
     if (!bids.length < 10) {
 	$$('bids-none').text('Nothing recent.').slideDown()
     }
@@ -72,61 +130,75 @@ var bidsReady = function(bids) {
 var bidsError = function(request, status, error) {
 }
 
+var bidsShow = function() {
+    smallMsg('Loading bids...')
+    //new BidsLoader({suffix: id64View(), success: bidsReady, error: bidsError})
+}
+
+var bidsHide = function() {
+   $$('bids-show').fadeIn(function() {
+       $$('bids-inner').fadeOut(function() {
+	   $$('bids-hide').fadeOut()
+       })
+    })
+}
+
+
+//
+// listings
+//
+var listingsShow = function() {
+    smallMsg('Loading listings...')
+    new ListingsLoader({
+	suffix: id64View(), success: listingsReady, error: listingsError
+    })
+}
+
+var listingsHide = function() {
+   $$('listings-show').fadeIn(function() {
+       $$('listings-inner').fadeOut(function() {
+	   $$('listings-hide').fadeOut()
+       })
+    })
+}
+
+var listingsReady = function(listings) {
+    smallMsg('Listings loaded.')
+    $$('listings-show').fadeOut(function() {
+	$$('listings-inner').fadeIn(function() {
+	    $$('listings-hide').fadeIn()
+	})
+    })
+    if (!listings.length) {
+	$$('listings-none').text('Nothing recent.').slideDown()
+    } else {
+	listingsOkay(listings)
+    }
+}
+
+
+var listingsError = function(request, status, error) {}
+
+
+
+
+//
 // applies to non-auth, auth, and owner-auth
+//
 var playerProfileOkay = function(profile) {
     setTitle(profile.personaname)
     $$('title').text(profile.personaname)
     $$('avatar').attr('src', profile.avatarfull)
     $$('badge').slideDown()
     smallMsg().fadeAway()
+    $('.init-container').fadeIn()
 }
-
 
 var playerProfileError = function(request, status, error) {
     smallMsg().fadeAway()
 }
 
 
-var showBackpack = function(backpack) {
-    var schema = new SchemaTool()
-    var tipTool = new TooltipView(schema)
-    var bpNav = new BackpackNavigator('profile')
-    var bpTool = new BackpackItemsTool(backpack,[], [], 'profile')
-    var hoverItem = function(e) {
-        tipTool.show(e)
-        try {
-            var data = $('img', this).data('node')
-        	if (!data.flag_cannot_trade) {
-	            $(this).addClass('outline')
-                }
-        } catch (e) {}
-    }
-    var unhoverItem = function(e) {
-        tipTool.hide(e)
-        $(this).removeClass('outline')
-    }
-    bpNav.init()
-    bpTool.init()
-    schema.setImages()
-    $('#profile-backpack td').hover(hoverItem, unhoverItem)
-    $$('backpack').fadeIn()
-    $('#profile-backpack').width($('#backpack-profile table.backpack tbody').first().width()).addClass('centered')
-}
-
-
-var backpackReady = function(backpack) {
-    smallMsg('Backpack loaded.')
-    $$('backpack-wrapper').slideDown()
-    $$('backpack-show').click(function () {
-	$$('backpack-show').fadeOut('fast', function() {
-	    showBackpack(backpack)
-	})
-    })
-}
-
-var backpackError = function(request, status, error) {
-    smallMsg('Backpack loaded.')
-}
 
 
 // auth profile -> maybe load other player profile, load (bids+backpack+listings)
@@ -144,9 +216,7 @@ var authProfileOkay = function(profile) {
     }
     showProfile(profile)
     smallMsg('Loading backpack...')
-    new BackpackLoader({suffix: id64, success: backpackReady, error:backpackError})
-    new BidsLoader({suffix: id64, success: bidsReady, error: bidsError})
-    new ListingsLoader({suffix: id64, success: listingsReady, error: listingsError})
+    //new BidsLoader({suffix: id64, success: bidsReady, error: bidsError})
 }
 
 
@@ -156,9 +226,7 @@ var authProfileError = function(request, status, error) {
     if (request.status==401) {
 	setHeadings()
 	smallMsg('Loading profile...')
-	new BackpackLoader({suffix: id64, success: backpackReady, error:backpackError})
-	new BidsLoader({suffix: id64, success: bidsReady, error: bidsError})
-	new ListingsLoader({suffix: id64, success: listingsReady, error: listingsError})
+	//new BidsLoader({suffix: id64, success: bidsReady, error: bidsError})
 	new ProfileLoader({suffix: id64, success: playerProfileOkay, error: playerProfileError})
     }
 }
@@ -171,6 +239,23 @@ var schemaReady = function(s) {
 
 // document loaded -> load schema
 $(document).ready(function() {
+    $$('backpack-hide').click(backpackHide)
+    $$('backpack-show').click(backpackShow)
+    $$('bids-hide').click(bidsHide)
+    $$('bids-show').click(bidsShow)
+    $$('listings-hide').click(listingsHide)
+    $$('listings-show').click(listingsShow)
+
     smallMsg('Loading...')
     new SchemaLoader({success: schemaReady})
+
+    if (window.location.search.indexOf("show=listings")>-1) {
+	$$('listings-show').click()
+    }
+
+    if (window.location.search.indexOf("show=backpack")>-1) {
+	$$('backpack-show').click()
+    }
+
+
 })
